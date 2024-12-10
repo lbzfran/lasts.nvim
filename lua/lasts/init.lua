dcda8d73e54fc29178e57034affda94fbb5ad711
Liam Bagabag <lbscout1@gmail.com>
2024-12-10 12:55:08 -0800

    renamed project. added read and readall feature as vim cmd.

---
+++ lua/lasts/init.lua
@@ -0,0 +1,48 @@
+local M = {}
+
+M.var = {}
+M.default_savefile = debug.getinfo(1, "S").source:sub(2):match("^(.*[/\\])") .. "savefile.csv"
+
+local csv = require("lasts.simplecsv")
+
+M.save = function()
+    csv.write(M.savefile, M.var)
+end
+
+M.load = function()
+    M.var = csv.read(M.savefile)
+end
+
+M.read = function(key)
+    return M.var[key]
+end
+
+M.readall = function()
+    print("Key-Value Pairs currently stored:")
+    for key, val in pairs(M.var) do
+        print(key .. ": " .. val)
+    end
+end
+
+M.setup = function()
+    M.savefile = M.savefile or M.default_savefile
+    if not csv.exists(M.savefile) then
+        os.execute("touch " .. M.savefile)
+    end
+    M.load()
+
+    vim.api.nvim_create_user_command("Lasts", function(opts)
+        if next(opts.fargs) ~= nil then
+            local res = M.read(opts.args)
+            if res ~= nil then
+                print(opts.args .. ": " .. res)
+            else
+                print(opts.args .. ": nil")
+            end
+        else
+            M.readall()
+        end
+    end, { nargs = "*" })
+end
+
+return M
